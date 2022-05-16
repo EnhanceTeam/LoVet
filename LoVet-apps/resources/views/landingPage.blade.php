8073673106b14333bbe68a289f309dadcd22438b
Daniel Aprillio <danielaprillio02@gmail.com>
2022-05-16 17:44:09 +0700

    Update landingPage.blade.php

--- LoVet-apps/resources/views/landingPage.blade.php
+++ LoVet-apps/resources/views/landingPage.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +8,17 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="{{ asset('css/landingPage.css') }}">
     <title>LoVet</title>
 </head>
+
 <body>
+    {{-- Navigation Bar --}}
     <nav class="navbar navbar-expand-sm bg-yellow-custom sticky-top">
         <div class="container-fluid">
             <div class="navbar-brand fw-bold px-3">
@@ -32,16 +38,17 @@
                         </li>
                         <li class="nav-item me-4">
                             <a class="nav-link fw-bold text-black-custom" href="#">Feedback</a>
-                        </li>    
+                        </li>
                         <li class="nav-item me-4">
                             <a class="nav-link fw-bold text-black-custom" href="#">Contact Us</a>
-                        </li>  
+                        </li>
                     </ul>
                 </div>
             </div>
         </div>
     </nav>
 
+    {{-- Introduction --}}
     <div class="row align-items-center m-3 ms-5 ps-2" style="height: 80vh">
         <div class="col mx-4">
             <div class="d-flex justify-content-start">
@@ -58,5 +65,30 @@
             <img src="{{ asset('images/Aset/Decor Pic 1.png') }}" width="80%" height="80%">
         </div>
     </div>
+
+    {{-- Join Us --}}
+    <div class="row align-items-center" style="height: 80vh">
+        <div class="d-flex justify-content-center">
+            <h2 class="fw-bold">Join Us</h2>
+        </div>
+        <div class="d-flex flex-column">
+            <div class="d-flex justify-content-center">
+                <h5 class="fw-bold">Keep up with our latest update</h5>
+            </div>
+            <div class="d-flex justify-content-center">
+                <div class="row align-items-center m-2">
+                    <div class="col-auto">
+                        <input type="text" class="form-control" placeholder="Email">
+                    </div>
+                    <div class="col-auto">
+                        <button class="btn btn-warning hover-shadow">
+                            Join Now
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
