7f3b2bb6ac0e9ca6d646f49b40cb141b36100ca0
Timothy Kristanto <timothy01@student.ciputra.ac.id>
2022-11-25 21:59:07 +0700

    Fixed Lagged on Chat Sent

--- lovet-chat/src/services/firebase.js
+++ lovet-chat/src/services/firebase.js
@@ -7,13 +7,13 @@ import "firebase/compat/auth"
 import { useAuthState } from "react-firebase-hooks/auth"
 
 firebase.initializeApp({
-    apiKey: "AIzaSyBOz3j8jm6hHELqegXzLxycVcxlqNwE2Fs",
-    authDomain: "lovet-chat.firebaseapp.com",
-    projectId: "lovet-chat",
-    storageBucket: "lovet-chat.appspot.com",
-    messagingSenderId: "765869922982",
-    appId: "1:765869922982:web:e9f276ab5b4015621af267",
-    measurementId: "G-QPGWXV6LTX",
+  apiKey: "AIzaSyBOz3j8jm6hHELqegXzLxycVcxlqNwE2Fs",
+  authDomain: "lovet-chat.firebaseapp.com",
+  projectId: "lovet-chat",
+  storageBucket: "lovet-chat.appspot.com",
+  messagingSenderId: "765869922982",
+  appId: "1:765869922982:web:e9f276ab5b4015621af267",
+  measurementId: "G-QPGWXV6LTX",
 })
 
 const firestore = firebase.firestore()
