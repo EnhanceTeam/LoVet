a3211431831eb7937003f7b19e2e5fc4c4d2d135
Timothy Kristanto <timothy01@student.ciputra.ac.id>
2022-12-07 14:26:10 +0700

    Test

--- lovet-chat/src/views/JoinRoom/JoinRoom.jsx
+++ lovet-chat/src/views/JoinRoom/JoinRoom.jsx
@@ -7,108 +7,114 @@ import { LoadingButton } from "@mui/lab"
 import { buttonTheme } from "../../themes/theme"
 import lovetLogoHorizontalTransparent from "../../assets/svg/lovet_logo_horizontal_transparent.svg"
 import "./join-room.css"
+import { UserAuth } from "../../context/AuthContext"
 
 const JoinRoom = () => {
-    const roomRef = fb.firestore.collection("Rooms")
-    const [roomIDInput, setRoomIDInput] = useState()
-    const [roomIdSnackbar, setRoomIdSnackbar] = useState()
-    const [loadingState, setLoadingState] = useState(false)
-    const [snackbarState, setSnackbarState] = useState(false)
-    const navigate = useNavigate()
+  const [roomIDInput, setRoomIDInput] = useState()
+  const [roomIdSnackbar, setRoomIdSnackbar] = useState()
+  const [loadingState, setLoadingState] = useState(false)
+  const [snackbarState, setSnackbarState] = useState(false)
+  const { user } = UserAuth()
+  const navigate = useNavigate()
+  const roomRef = fb.firestore.collection("Rooms")
 
-    const handleSnackbarClose = (event, reason) => {
-        if (reason === "clickaway") {
-            return
-        }
-
-        setSnackbarState(false)
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return
     }
 
-    const handleEnterChatClick = (e) => {
-        if (!roomIDInput) {
-            return
-        }
+    setSnackbarState(false)
+  }
 
-        e.preventDefault()
-
-        setLoadingState(true)
-        fb.firestore
-            .collection("Rooms")
-            .doc(roomIDInput)
-            .get()
-            .then((doc) => {
-                if (doc.exists) {
-                    // todo: satu room hanya bisa untuk 1 akun user dan 1 akun dokter
-                    navigate(`chatroom/${doc.id}`)
-                } else {
-                    setRoomIdSnackbar(roomIDInput)
-                    setSnackbarState(true)
-                }
-                setLoadingState(false)
-            })
+  const handleEnterChatClick = (e) => {
+    if (!roomIDInput) {
+      return
     }
 
-    return (
-        <div className="join_room_main">
-            <Snackbar
-                anchorOrigin={{ vertical: "top", horizontal: "center" }}
-                open={snackbarState}
-                autoHideDuration={6000}
-                onClose={handleSnackbarClose}
-            >
-                <Alert
-                    onClose={handleSnackbarClose}
-                    severity="warning"
-                    sx={{ width: "100%" }}
-                >
-                    Ruang chat dengan room ID "{roomIdSnackbar}" tidak ditemukan
-                </Alert>
-            </Snackbar>
-            <div className="join_room_center">
-                <div className="join_room_center_top">
-                    <div className="join_room_center_top_left"></div>
-                    <div className="join_room_center_top_center">
-                        <img
-                            className="join_room_logo"
-                            src={lovetLogoHorizontalTransparent}
-                            alt="Lovet logo"
-                        />
-                    </div>
-                    <div className="join_room_center_top_right">
-                        <Logout />
-                    </div>
-                </div>
-                <div className="join_room_center_center">
-                    <div className="join_room_center_center_text">
-                        <h2>Silahkan masukkan room ID</h2>
-                    </div>
-                    <TextField
-                        className="rounded_outlined_text_field"
-                        variant="outlined"
-                        placeholder="Room ID"
-                        value={roomIDInput}
-                        onChange={(e) => setRoomIDInput(e.target.value)}
-                        size="small"
-                        fullWidth
-                        autoFocus
-                    />
-                    <ThemeProvider theme={buttonTheme}>
-                        <LoadingButton
-                            className="rounded_button"
-                            onClick={handleEnterChatClick}
-                            variant="contained"
-                            disableElevation
-                            fullWidth
-                            loading={loadingState}
-                        >
-                            Masuk ruang chat
-                        </LoadingButton>
-                    </ThemeProvider>
-                </div>
-                <div className="join_room_center_bottom"></div>
-            </div>
+    e.preventDefault()
+
+    setLoadingState(true)
+    fb.firestore
+      .collection("Rooms")
+      .doc(roomIDInput)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          // todo: satu room hanya bisa untuk 1 akun user dan 1 akun dokter
+
+          navigate(`chatroom/${doc.id}`)
+        } else {
+          setRoomIdSnackbar(roomIDInput)
+          setSnackbarState(true)
+        }
+        setLoadingState(false)
+      })
+      .catch((error) => {
+        setLoadingState(false)
+      })
+  }
+
+  return (
+    <div className="join_room_main">
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={snackbarState}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+      >
+        <Alert
+          onClose={handleSnackbarClose}
+          severity="warning"
+          sx={{ width: "100%" }}
+        >
+          Ruang chat dengan room ID "{roomIdSnackbar}" tidak ditemukan
+        </Alert>
+      </Snackbar>
+      <div className="join_room_center">
+        <div className="join_room_center_top">
+          <div className="join_room_center_top_left"></div>
+          <div className="join_room_center_top_center">
+            <img
+              className="join_room_logo"
+              src={lovetLogoHorizontalTransparent}
+              alt="Lovet logo"
+            />
+          </div>
+          <div className="join_room_center_top_right">
+            <Logout />
+          </div>
         </div>
-    )
+        <div className="join_room_center_center">
+          <div className="join_room_center_center_text">
+            <h2>Silahkan masukkan room ID</h2>
+          </div>
+          <TextField
+            className="rounded_outlined_text_field"
+            variant="outlined"
+            placeholder="Room ID"
+            value={roomIDInput}
+            onChange={(e) => setRoomIDInput(e.target.value)}
+            size="small"
+            fullWidth
+            autoFocus
+          />
+          <ThemeProvider theme={buttonTheme}>
+            <LoadingButton
+              className="rounded_button"
+              onClick={handleEnterChatClick}
+              variant="contained"
+              disableElevation
+              fullWidth
+              loading={loadingState}
+            >
+              Masuk ruang chat
+            </LoadingButton>
+          </ThemeProvider>
+        </div>
+        <div className="join_room_center_bottom"></div>
+      </div>
+    </div>
+  )
 }
 
 export default JoinRoom
--- lovet-chat/src/views/ProtectedRoutes.jsx
+++ lovet-chat/src/views/ProtectedRoutes.jsx
@@ -1,9 +1,14 @@
 import { Login } from "@mui/icons-material"
 import React, { useEffect } from "react"
 import { UserAuth } from "../context/AuthContext"
+import { useParams } from "react-router-dom"
+import { findByAltText } from "@testing-library/react"
+import Menu from "./Menu"
+import fb from "../services/firebase"
 
 const ProtectedRoutes = ({ children }) => {
   const { user } = UserAuth()
+  const { roomID } = useParams()
   // kalo login jadi admin ttp ga bisa masuk
 
   // if (roomID !== null) {
@@ -16,6 +21,10 @@ const ProtectedRoutes = ({ children }) => {
 
   if (!user) {
     return <Login />
+  }
+
+  if (!fb.firestore.collection("Rooms").doc(roomID).get()) {
+    return <Menu />
   }
 
   return children
