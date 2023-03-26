6161603b949fa2b91098b5c4138149748344f0e6
Bryan <71095749+bryanless@users.noreply.github.com>
2023-03-26 21:24:22 +0700

    Implement why us

--- lovet-chat/src/views/LandingPage/LandingPage.jsx
+++ lovet-chat/src/views/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import NavBar from "../Common/Components/NavBar"
 import Home from "./Components/Home"
+import WhyUs from "./WhyUs/WhyUs"
 
 const LandingPage = () => {
   // window.location.href = "https://lovetindonesia.wixsite.com/home"
@@ -8,6 +9,7 @@ const LandingPage = () => {
     <>
       <NavBar />
       <Home />
+      <WhyUs />
     </>
   )
 }
